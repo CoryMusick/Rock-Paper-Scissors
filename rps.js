0929392901bbf21d974c4e5fdffaa5525488efaa
Cory Musick <cory.musick37@gmail.com>
2021-08-12 20:09:54 -0500

    rps game with simple ui

--- rps.js
+++ rps.js
@@ -1,10 +1,34 @@
-console.log("test")
-
+// Declaring Variables
 const choices = ["Rock", "Paper", "Scissors"]
+let buttonId = "";
+let computerChoice = "";
+let playerChoice = "";
+let playerWin = 0;
+let computerWin = 0;
 
+// Gettting Dom Elements
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector("#results-container");
+const computerWinContainer = document.querySelector("#computer-win");
+const playerWinContainer = document.querySelector("#player-win");
+const computerSelectionContainer = document.querySelector("#computer-selection");
+const playerSelectionContainer =  document.querySelector("#player-selection");
 
-function playerSelection() {
-    let playerChoice = prompt("Rock, Paper, Scissors");
+// Adding Event Listners to Buttons
+buttons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        if(playerWin < 5 && computerWin < 5){
+            game(playerSelection(button.id), computerSelection())  
+        }
+        if(button.id == "reset"){
+          reset();
+        }
+    } )
+});
+
+// Player and Computer Selections
+function playerSelection(int) {
+    let playerChoice = choices[int]
     return playerChoice.toLowerCase()
 }
 
@@ -13,36 +37,57 @@ function computerSelection() {
     return computerChoice.toLowerCase()
 }
 
+//Reset Button Functionality
+function reset() {
+    computerWinContainer.innerHTML = "Computer Wins: 0"
+    playerWinContainer.innerHTML = "Player Wins: 0"
+    playerSelectionContainer.innerHTML = "Player Selection: 0"
+    computerSelectionContainer.innerHTML = "Computer Selection: 0"
+    results.innerHTML = "Result:"
+    playerWin = 0;
+    computerWin = 0;
+}
+// Game Function
 function game(playerSelection, computerSelection) {
+   playerSelectionContainer.innerHTML = "Player Selection: " + playerSelection;
+    computerSelectionContainer.innerHTML = "Computer Selection: " + computerSelection
     // tie
     if( playerSelection == computerSelection){
-        console.log("It was a tie.")
+        
     }
     // rock beats scissors
     if(playerSelection == "rock" && computerSelection == "scissors"){
-        console.log("You Win! Rock beats Scissors")
+        
+        playerWin++
     } else if(computerSelection == "rock" && playerSelection == "scissors"){
-        console.log("You Lose! Rock beats Scissors")
+        computerWin++
     }
 
     // Paper beats rock
     if(playerSelection == "paper" && computerSelection == "rock"){
-        console.log("You Win! Paper beats Rock")
+        playerWin++
     } else if(computerSelection == "paper" && playerSelection == "rock"){
-        console.log("You Lose! Paper beats Rock")
+        computerWin++
     }
 
     //scissors beats paper
     if(playerSelection == "scissors" && computerSelection == "paper"){
-        console.log("You Win! Scissors beats Paper")
+        playerWin++
     } else if(computerSelection == "scissors" && playerSelection == "paper"){
-        console.log("You Loser! Scissors beats Paper")
+        computerWin++
     }
+  
+    //Update Player Win Totals
+    playerWinContainer.innerHTML = "Player Wins: " + playerWin.toString();
+    computerWinContainer.innerHTML = "Computer Wins: " + computerWin.toString();
 
+    //Check Win Max
+    if(playerWin >= 5){
+    results.innerHTML = "Result: You Win!"
+    
+    }else if (computerWin >= 5){
+    results.innerHTML = "Result: Computer Wins!"
+    
+    }
 }
 
-let playerChoice = playerSelection()
-let computerChoice = computerSelection()
-console.log(computerChoice)
-console.log(playerChoice)
-game(playerChoice, computerChoice);
